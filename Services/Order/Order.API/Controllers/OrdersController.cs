e2b2f95728177074195308a0e32eeb0d8dcff8e0
Journey <Journey@LAPTOP-80B8G1I7>
2024-05-22 16:37:52 +0300

    Add project files.

---
+++ Services/Order/Order.API/Controllers/OrdersController.cs
@@ -0,0 +1,78 @@
+﻿using MassTransit;
+using MassTransit.Transports;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Order.API.DTOs.Order.API.DTOs;
+using Order.API.Model;
+using Shared;
+
+namespace Order.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrdersController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+        private readonly IPublishEndpoint _publishEndpoint;
+        public OrdersController(AppDbContext context, IPublishEndpoint publishEndpoint)
+        {
+            _context = context;
+            _publishEndpoint = publishEndpoint;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(OrderCreateDto orderCreate)
+        {
+            try
+            {
+                var newOrder = new Model.Order
+                {
+                    BuyerId = orderCreate.BuyerId,
+                    Status = OrderStatus.Suspend,
+                    Address = new Address { Line = orderCreate.Address.Line, Province = orderCreate.Address.Province, District = orderCreate.Address.District },
+                    CreatedDate = DateTime.Now,
+                    FailMessage = "test"
+                };
+
+                orderCreate.orderItems.ForEach(item =>
+                {
+                    newOrder.Items.Add(new OrderItem() { Price = item.Price, ProductId = item.ProductId, Count = item.Count });
+                });
+
+                await _context.AddAsync(newOrder);
+
+                await _context.SaveChangesAsync();
+
+
+                var orderCreatedEvent = new OrderCreatedEvent()
+                {
+                    BuyerId = orderCreate.BuyerId,
+                    OrderId = newOrder.Id,
+                    Payment = new PaymentMessage
+                    {
+                        CardName = orderCreate.payment.CardName,
+                        CardNumber = orderCreate.payment.CardNumber,
+                        Expiration = orderCreate.payment.Expiration,
+                        CVV = orderCreate.payment.CVV,
+                        TotalPrice = orderCreate.orderItems.Sum(x => x.Price * x.Count)
+                    },
+                };
+
+                orderCreate.orderItems.ForEach(item =>
+                {
+                    orderCreatedEvent.orderItems.Add(new OrderItemMessage { Count = item.Count, ProductId = item.ProductId });
+                });
+
+                await _publishEndpoint.Publish(orderCreatedEvent);
+
+                return Ok();
+            }
+            catch (Exception e)
+            {
+                return Ok();
+
+            }
+
+        }
+    }
+}
