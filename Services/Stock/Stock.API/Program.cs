a994273a47bfadf4e96c693469fa4d7261d9ce1d
muhammed1412 <muhammed@retrace.ai>
2024-05-23 08:34:36 +0300

    test

--- Services/Stock/Stock.API/Program.cs
+++ Services/Stock/Stock.API/Program.cs
@@ -1,4 +1,11 @@
+using MassTransit;
+using Microsoft.EntityFrameworkCore;
+using Shared;
+using Stock.API.Consumer;
+using Stock.API.Model;
+
 var builder = WebApplication.CreateBuilder(args);
+
 
 // Add services to the container.
 
@@ -6,6 +13,22 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<AppDbContext>(options =>
+{
+    options.UseInMemoryDatabase("StockDb");
+});
+builder.Services.AddMassTransit(x =>
+{
+    x.AddConsumer<OrderCreatedEventConsumer>();
+    x.UsingRabbitMq((context, cfg) =>
+    {
+        cfg.Host(builder.Configuration.GetConnectionString("RabbitMQ"));
+        cfg.ReceiveEndpoint(RabbitMQSettingsConst.StockOrderCreatedEventQueueName, e =>
+        {
+            e.ConfigureConsumer<OrderCreatedEventConsumer>(context);
+        });
+    });
+});
 
 var app = builder.Build();
 
@@ -22,4 +45,25 @@ app.UseAuthorization();
 
 app.MapControllers();
 
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    var context = services.GetRequiredService<AppDbContext>();
+    SeedDatabase(context); 
+}
+
+
 app.Run();
+
+void SeedDatabase(AppDbContext context)
+{
+    if (!context.Stocks.Any())
+    {
+        context.Stocks.AddRange(new Stock.API.Model.Stock[]
+        {
+            new Stock.API.Model.Stock { Id = 1, ProductId = 1,Count=100 },
+            new Stock.API.Model.Stock { Id = 122, ProductId = 122,Count=100 },
+        });
+        context.SaveChanges();
+    }
+}
