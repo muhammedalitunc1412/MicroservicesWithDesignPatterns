be6089ff1bcad229eccca3e405349b557cd5c88a
muhammed1412 <muhammed@retrace.ai>
2024-05-23 13:39:02 +0300

    update

--- Services/Payment/Payment.API/Program.cs
+++ Services/Payment/Payment.API/Program.cs
@@ -1,3 +1,7 @@
+using MassTransit;
+using Payment.API.Consumer;
+using Shared;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +10,19 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddMassTransit(x =>
+{
+    x.AddConsumer<StockReservedEventConsumer>();
+    x.UsingRabbitMq((context, cfg) =>
+    {
+        cfg.Host(builder.Configuration.GetConnectionString("RabbitMQ"));
+        cfg.ReceiveEndpoint(RabbitMQSettingsConst.StockReservedEventQueueName, e =>
+        {
+            e.ConfigureConsumer<StockReservedEventConsumer>(context);
+        });
+    });
+});
 
 var app = builder.Build();
 
